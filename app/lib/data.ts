700113b734a63ff446931e83563e479a4965796f
abhinavjha29 <abhinav_29@outlook.com>
2024-05-27 11:06:58 +0530

    fix isse

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,7 +9,6 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
-import axios from 'axios';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
@@ -22,11 +21,11 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await axios.get('http://localhost:3000/revenue');
+    const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed');
+    // console.log('Data fetch completed after 3 seconds.');
 
-    return data.data;
+    return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
@@ -35,17 +34,17 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    const response = await axios.get(
-      'http://localhost:3000/invoices?_start=0&_limit=5',
-    );
+    const data = await sql<LatestInvoiceRaw>`
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      ORDER BY invoices.date DESC
+      LIMIT 5`;
 
-    const data = response.data;
-
-    const latestInvoices = data.map((invoice) => ({
+    const latestInvoices = data.rows.map((invoice: LatestInvoiceRaw) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
-
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -58,14 +57,23 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
-    const response = await axios.get('http://localhost:3000/invoices');
-    const invoiceData = response.data;
-    const customerResponse = await axios.get('http://localhost:3000/customers');
-    const customerData = customerResponse.data;
-    const numberOfInvoices = Number(invoiceData.count ?? '0');
-    const numberOfCustomers = Number(customerData.count ?? '0');
-    const totalPaidInvoices = formatCurrency(invoiceData.paid ?? '0');
-    const totalPendingInvoices = formatCurrency(invoiceData.pending ?? '0');
+    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+    const invoiceStatusPromise = sql`SELECT
+         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+         FROM invoices`;
+
+    const data = await Promise.all([
+      invoiceCountPromise,
+      customerCountPromise,
+      invoiceStatusPromise,
+    ]);
+
+    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
+    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
+    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
+    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
 
     return {
       numberOfCustomers,
